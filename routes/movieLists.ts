cb5ee2ec803b89ffc0c4b158e6ddd9bbeac84e3a
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-24 23:35:01 -0800

    Add top 200 titles route for search

--- routes/movieLists.ts
+++ routes/movieLists.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import axios from "axios";
 import { getSubtractedDate } from "../utils/timeUtils";
 import { getFormattedDate } from "../utils/formatUtils";
 import { handleRoute } from "./common";
+import { BASE_URL } from "../index";
 
 const moviesListRouter = express.Router();
 const sixMonthsAgoFormatted = getFormattedDate(getSubtractedDate(new Date(), 0, 6, 0));
@@ -31,6 +33,33 @@ moviesListRouter.get("/movies/discover",
                 language: "en-US",
                 sort_by: "popularity.desc"
             });
+    });
+
+moviesListRouter.get("/movies/top_200_titles",
+    async (request, response) => {
+        try {
+            const popularMovieTitles: string[] = [];
+            for (let page = 1; page <= 10; page++) {
+                const res = await axios.get(`${BASE_URL}discover/movie`, {
+                    params: {
+                        include_adult: false,
+                        language: "en-US",
+                        page: page,
+                        sort_by: "popularity.desc"
+                    },
+                    headers: {
+                        Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+                    }
+                });
+                if (res !== null) {
+                    const popularMoviesArray = res.data["results"];
+                    popularMoviesArray.forEach((resObj: any) => popularMovieTitles.push(resObj["title"]));
+                }
+            }
+            response.send(popularMovieTitles);
+        } catch (err) {
+            console.log(err);
+        }
     });
 
 moviesListRouter.get("/movies/now_playing",
