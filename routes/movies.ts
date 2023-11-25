59dcffa8ca648c34f607ddd40a7483ce16b94788
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-24 16:02:07 -0800

    Change endpoint path; Convert route handler to async await

--- routes/movies.ts
+++ routes/movies.ts
@@ -4,13 +4,18 @@ import { BASE_URL } from "../index";
 
 const moviesListRouter = express.Router();
 
-moviesListRouter.get("/now_playing/:page?", (request: Request, response: Response) => {
+moviesListRouter.get("/movies/now_playing/:page?", async (request: Request, response: Response) => {
     const page = request.params.page ?? 1;
-    axios.get(`${BASE_URL}movie/now_playing?language=en-US&page=${page}`, {
-        headers: {
-            "Authorization": `Bearer ${process.env.TMDB_API_KEY}`
-        }
-    }).then(res => response.send(res.data)).catch(err => console.log(err));
+    try {
+        const res = await axios.get(`${BASE_URL}movie/now_playing?language=en-US&page=${page}`, {
+            headers: {
+                "Authorization": `Bearer ${process.env.TMDB_API_KEY}`
+            }
+        });
+        response.send(res.data);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 export default moviesListRouter;
