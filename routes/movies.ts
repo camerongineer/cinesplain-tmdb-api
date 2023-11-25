e6b9cc3ed2bb99ec1ecc8e0ede0724cf99f6c531
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-24 16:57:02 -0800

    Add more endpoints; Refactor

--- routes/movies.ts
+++ routes/movies.ts
@@ -7,10 +7,16 @@ import { getSubtractedDate } from "../utils/timeUtils";
 const moviesListRouter = express.Router();
 const sixMonthsAgoFormatted = getFormattedDate(getSubtractedDate(new Date(), 0, 6, 0));
 
-moviesListRouter.get("/movies/now_playing/:page?", async (request: Request, response: Response) => {
-    const page = request.params.page ?? 1;
+const handleRoute = async (request: Request, response: Response, endpoint: string, params: any) => {
     try {
-        const res = await axios.get(`${BASE_URL}movie/now_playing?language=en-US&page=${page}`, {
+        const res = await axios.get(`${BASE_URL}${endpoint}`, {
+            params: {
+                ...params,
+                page: request.query.page ?? 1,
+                include_adult: false,
+                language: 'en-US',
+                with_original_language: 'en'
+            },
             headers: {
                 Authorization: `Bearer ${process.env.TMDB_API_KEY}`
             }
@@ -19,35 +25,68 @@ moviesListRouter.get("/movies/now_playing/:page?", async (request: Request, resp
     } catch (err) {
         console.log(err);
     }
-});
+}
 
-moviesListRouter.get("/movies/most_loved/:page?", async (request: Request, response: Response) => {
-    const page = request.params.page ?? 1;
-    try {
-        const res = await axios.get(`${BASE_URL}discover/movie?include_adult=false&language=en-US&with_original_language=en&page=${page}&sort_by=vote_average.desc&primary_release_date.gte=${sixMonthsAgoFormatted}&without_genres=99,10755&vote_count.gte=20`, {
-            headers: {
-                Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+moviesListRouter.get("/movies/search",
+    (request, response) => {
+        handleRoute(
+            request,
+            response,
+            "search/movie",
+            {
+                query: request.query.query,
+                sort_by: "popularity.desc"
             }
-        });
-        response.send(res.data);
-    } catch (err) {
-        console.log(err);
-    }
-});
+        );
+    });
 
+moviesListRouter.get("/movies/now_playing",
+    (request, response) => {
+        handleRoute(request,
+            response,
+            "movie/now_playing",
+            {});
+    });
 
-moviesListRouter.get("/movies/most_hated/:page?", async (request: Request, response: Response) => {
-    const page = request.params.page ?? 1;
-    try {
-        const res = await axios.get(`${BASE_URL}discover/movie?include_adult=false&language=en-US&with_original_language=en&page=${page}&sort_by=vote_average.asc&primary_release_date.gte=${sixMonthsAgoFormatted}&without_genres=99,10755&vote_count.gte=20`, {
-            headers: {
-                Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-            }
+moviesListRouter.get("/movies/upcoming",
+    (request, response) => {
+        handleRoute(request,
+            response,
+            "discover/movie",
+            { sort_by: "popularity.desc", primary_release_date: { gte: getFormattedDate(new Date()) } });
+    });
+
+moviesListRouter.get("/movies/classics",
+    (request, response) => {
+        handleRoute(request,
+            response,
+            "discover/movie",
+            {
+                sort_by: "vote_average.desc",
+                without_genres: [99, 10755],
+                vote_count: { gte: 1000 }
+            });
+    });
+
+moviesListRouter.get("/movies/most_loved",
+    (request, response) => {
+        handleRoute(request, response, "discover/movie", {
+            sort_by: "vote_average.desc",
+            primary_release_date: { gte: sixMonthsAgoFormatted },
+            without_genres: [99, 10755],
+            vote_count: { gte: 20 }
         });
-        response.send(res.data);
-    } catch (err) {
-        console.log(err);
-    }
-});
+    });
+
+moviesListRouter.get("/movies/most_hated",
+    (request, response) => {
+        handleRoute(request, response, "discover/movie", {
+            sort_by: "vote_average.asc",
+            primary_release_date: { gte: sixMonthsAgoFormatted },
+            without_genres: [99, 10755],
+            vote_count: { gte: 20 }
+        });
+    });
+
 
 export default moviesListRouter;
